eb4d633378473ad7cc96f1ff86e677824c0d867a
magnitopic <magnitopic@gmail.com>
2022-11-17 12:39:33 +0100

    header now functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,18 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:21:18 by alaparic          #+#    #+#             */
-/*   Updated: 2022/11/16 18:23:58 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/11/17 12:18:34 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef FT_PRINTF_H
-#	define FT_PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include<stdarg.h>
 
-
+int	ft_printf(const char *test, ...);
 
 #endif

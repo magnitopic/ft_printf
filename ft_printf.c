172945bdc145e61d919242905c1036cde0c9e18f
magnitopic <magnitopic@gmail.com>
2022-11-17 14:04:11 +0100

    tests de printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,25 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:20:26 by alaparic          #+#    #+#             */
-/*   Updated: 2022/11/16 18:20:27 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/11/17 13:17:35 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+
+	va_start(ap, format);
+	printf("%s", format);
+	printf("%s", va_arg(ap, char *));
+	printf("%s", va_arg(ap, char *));
+	va_end(ap);
+}
+
+int	main(void)
+{
+	ft_printf("yes", "Hello ", "There");
+	return (0);
+}
